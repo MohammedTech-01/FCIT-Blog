5674f1a4255d18b1e562617996421d2854019649
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-01 17:43:54 +0300

    Fix W3C

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php include 'includes/header.php'; ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
 
 <head>
     <meta charset="UTF-8">
