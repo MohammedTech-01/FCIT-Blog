c5edf5c7a3a09b0f4ef201138ae897f0b4e9c4f4
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-01 17:51:55 +0300

    Fix W3C

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
+<?php include 'includes/header.php'; ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FCIT Blog</title>
+    <link rel="stylesheet" href="global/main.css">
+</head>
 
 <body>
     <div class="container">
