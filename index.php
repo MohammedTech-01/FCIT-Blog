4f105c707f167158cdb14b45387566c1a4174904
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-01 17:32:48 +0300

    Fix W3C

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <?php include 'includes/header.php'; ?>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FCIT Blog</title>
-    <link rel="stylesheet" href="global/main.css"> <!-- Adjust if necessary -->
-</head>
-
 
 <body>
     <div class="container">
@@ -18,7 +13,7 @@
             // Dynamically load the page based on the URL
             $page = $_GET['page'];
             $allowed_pages = ['home', 'resume', 'login', 'signup', 'services', 'about', 'contact', 'blog', 'fullblog', 'writeBlog', 'events', 'loginSignup', 'feedback', 'gallery_images']; // Allowed page names
-        
+
             if (in_array($page, $allowed_pages)) {
                 include "pages/{$page}.php"; // Dynamically include the requested page
             } else {
