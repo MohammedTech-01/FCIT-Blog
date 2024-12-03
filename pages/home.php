53e95f0c0532cba916473ffa6ab4779811cbf979
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-03 23:16:53 +0300

    sssss

--- pages/home.php
+++ pages/home.php
@@ -5,10 +5,10 @@
             <h1>Welcome to FCIT Blog</h1>
             <p>Explore the latest stories on FCIT and more.</p>
             <div>
-                <a href="index.php?page=blog" class="button-link">Read Our Latest Posts</a>
+                <a href="index.php?page=blog" class="button">Read Our Latest Posts</a>
             </div>
             <div>
-                <a href="index.php?page=resume" class="button-link">View My Resume</a>
+                <a href="index.php?page=resume" class="button">View My Resume</a>
             </div>
         </div>
     </div>
