c07d4f15f55b4b0d370cf7d35edb846149aba927
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-04 00:24:29 +0300

    Fix all errors

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,13 @@
 <div class="footer">
-        <p>© 2024 FCIT Blog. All Rights Reserved.</p>
-        <div class="validation">
-            <!-- W3C XHTML Validation -->
-            <a href="https://validator.w3.org/check?uri=https%3A%2F%2Fblog-app-fcit.up.railway.app%2F&charset=%28detect+automatically%29&doctype=XHTML+1.0+Strict&group=0&user-agent=W3C_Validator%2F1.3+#congrats">
-                <img src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0" />
-            </a>
-            <!-- W3C CSS Validation -->
-            <a href="https://jigsaw.w3.org/css-validator/check/referer">
-                <img style="border:0;width:88px;height:31px;" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" />
-            </a>
-        </div>
+    <p>© 2024 FCIT Blog. All Rights Reserved.</p>
+    <div class="validation">
+        <!-- W3C XHTML Validation -->
+        <a href="https://validator.w3.org/check?uri=https%3A%2F%2Fblog-app-fcit.up.railway.app%2F&amp;charset=%28detect+automatically%29&amp;doctype=XHTML+1.0+Strict&amp;group=0&amp;user-agent=W3C_Validator%2F1.3+#congrats">
+            <img src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0" />
+        </a>
+        <!-- W3C CSS Validation -->
+        <a href="https://jigsaw.w3.org/css-validator/check/referer">
+            <img style="border:0;width:88px;height:31px;" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" />
+        </a>
     </div>
+</div>
