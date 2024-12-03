1f72e7cefbd13d3da358f9ecf6b2b3b783eb7710
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-03 22:22:57 +0300

    Fix all xhtml

--- pages/about.php
+++ pages/about.php
@@ -1,9 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE XHTML 1.0 Strict">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
   <title>FCIT Blog</title>
 </head>
 
--- pages/fullblog.php
+++ pages/fullblog.php
@@ -27,10 +27,14 @@ if ($result->num_rows === 0) {
 $blog = $result->fetch_assoc();
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE XHTML 1.0 Strict">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <title><?php echo htmlspecialchars($blog['title']); ?> - FCIT Blog</title>
 </head>
 <body>
