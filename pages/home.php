bfa91e542aba1bfe0504a901d1d081fadf691051
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-03 22:45:07 +0300

    sssss

--- pages/home.php
+++ pages/home.php
@@ -1,29 +1,27 @@
-<!DOCTYPE XHTML 1.0 Strict">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-  <title>FCIT Blog</title>
+    <title>FCIT Blog</title>
 </head>
 
 <body>
-  <!-- Hero Section (Home) -->
-  <section class="hero">
-    <div class="hero-text">
-      <h1>Welcome to FCIT Blog</h1>
-      <p>Explore the latest stories on FCIT and more.</p>
-      <a href="index.php?page=blog">
-        <button type="button">Read Our Latest Posts</button>
-      </a>
-      <!-- Button to go to the Resume page -->
-      <a href="index.php?page=resume">
-        <button type="button">View My Resume</button>
-      </a>
+    <!-- Hero Section (Home) -->
+    <div class="hero">
+        <div class="hero-text">
+            <h1>Welcome to FCIT Blog</h1>
+            <p>Explore the latest stories on FCIT and more.</p>
+            <a href="index.php?page=blog">
+                <button type="button">Read Our Latest Posts</button>
+            </a>
+            <!-- Button to go to the Resume page -->
+            <a href="index.php?page=resume">
+                <button type="button">View My Resume</button>
+            </a>
+        </div>
     </div>
-  </section>
 </body>
 
 </html>
