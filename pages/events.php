6d103624b1e41c4dac6371054386b2f5edfc994b
MohammedTech-01 <mohamedalluhaidan@gmail.com>
2024-12-04 00:16:53 +0300

    Fix events

--- pages/events.php
+++ pages/events.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title>FCIT Blog</title>
-    <link rel="stylesheet" href="global/print.css" />
+    <!-- <link rel="stylesheet" href="global/print.css" /> -->
 </head>
 <body>
     <div class="events-container">
